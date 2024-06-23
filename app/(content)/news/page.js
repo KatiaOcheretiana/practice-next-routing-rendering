e10be1a985ffb36a6c76fb6daa25478770e7fb43
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-06-23 14:52:26 +0300

    add backend

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,12 +1,66 @@
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { getAllNews } from "@/lib/news";
 import React from "react";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const news = await getAllNews();
+
   return (
     <>
       <h2>News Page</h2>
-      <NewsList listData={DUMMY_NEWS} />
+      <NewsList listData={news} />
     </>
   );
 }
+
+// CLIENT SIDE
+
+// "use client";
+
+// import NewsList from "@/components/news-list";
+// import React, { useEffect, useState } from "react";
+
+// export default function NewsPage() {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState("");
+//   const [news, setNews] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchNews() {
+//       setIsLoading(true);
+//       const response = await fetch("http://localhost:8080/news");
+
+//       if (!response.ok) {
+//         setError("Failed to fetch news.");
+//         setIsLoading(false);
+//       }
+
+//       const news = await response.json();
+//       setIsLoading(false);
+//       setNews(news);
+//     }
+
+//     fetchNews();
+//   }, []);
+
+//   if (isLoading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   if (error) {
+//     return <p>{error}</p>;
+//   }
+
+//   let newsContent;
+
+//   if (news) {
+//     newsContent = <NewsList listData={news} />;
+//   }
+
+//   return (
+//     <>
+//       <h2>News Page</h2>
+//       {newsContent}
+//     </>
+//   );
+// }
